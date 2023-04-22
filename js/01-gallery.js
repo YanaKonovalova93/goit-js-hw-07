b4a747d326ff7b48c92609b5d24509c929004033
YanaKonovalova93 <mikobacteria@gmail.com>
2023-04-22 20:05:09 +0300

    +

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -5,6 +5,8 @@ console.log(galleryItems);
 
 const ulGalleryEl = document.querySelector(".gallery");
 
+const body = document.querySelector("body");
+
 const markup = galleryItems
   .map(
     ({ preview, original, description }) => `<li class="gallery__item">
@@ -30,6 +32,29 @@ function onOpenModal(e) {
 
     const selectedUrl = e.target.dataset.source;
 
+    body.innerHTML = `<template>
+  <div class="modal">
+   <li class="gallery__item">
+     <a class="gallery__link" href="${selectedUrl}">
+      <img
+        class="gallery__image"
+        src="${selectedUrl}"
+         data-source="${selectedUrl}"
+        alt="q"
+        width="800" height="600"
+       />
+    </a>
+   </li>
+  </div>
+</template>`;
+    
+    const instance = basicLightbox.create(document.querySelector("template"));
+
+    instance.show();
+
+}
+
+
 //   const instance = basicLightbox.create(
 //     ` <img
 //         class="gallery__image"
@@ -42,13 +67,11 @@ function onOpenModal(e) {
 //        }
 //   );
     
-   return basicLightbox
-      .create(
-        ` <img
-        src="${selectedUrl}"
-        width="800" height="600"
-       />`
-      )
-      .show();
-
-}
+//    const instance = basicLightbox
+//      .create(
+//        ` <img
+//         src="${selectedUrl}"
+//         width="800" height="600"
+//        />`
+//      )
+//      .show();
